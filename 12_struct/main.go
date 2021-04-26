95ffe7eecd6940f7a0e830dd4a61f12e6566eccb
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-26 11:17:22 +0000

    (feat): ptr recievers

--- 12_struct/main.go
+++ 12_struct/main.go
@@ -7,15 +7,31 @@ import (
 
 // define person struct
 type Person struct {
-	firstName string
-	lastName  string
-	age       int
-	gender    string
+	// firstName string
+	// lastName  string
+	// age       int
+	// gender    string
+	firstName, lastName, gender string
+	age                         int
 }
 
 // greeting method (value reciever)
 func (p Person) greet() string {
 	return "Hello, my name is " + p.firstName + " " + p.lastName + " and I am " + strconv.Itoa(p.age) + " years old."
+}
+
+// has Bday (ptr reciever)
+func (p *Person) hasBday() {
+	p.age++
+}
+
+// getMarried (ptr reciever)
+func (p *Person) getMarried(spouceLastName string) {
+	if p.gender == "F" {
+		p.lastName = spouceLastName
+	} else {
+		return
+	}
 }
 
 func main() {
@@ -27,9 +43,18 @@ func main() {
 	// get single field
 	fmt.Println(p1.firstName)
 
-	// change value
-	p1.age++
+	// change value with ptr reciever
+	p1.hasBday()
+
+	// create another person
+	p2 := Person{"Samantha", "Smith", 20, "F"}
+	fmt.Println(p2.greet())
 
 	// value reciever
 	fmt.Println(p1.greet())
+
+	p1.getMarried("Smith")
+	p2.getMarried("Robinson")
+
+	fmt.Println(p2.greet())
 }
