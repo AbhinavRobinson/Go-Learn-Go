ac05ecffe206c97e5b373e8ca5175898c8231aeb
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-26 10:27:59 +0000

    (feat): add maps

---
+++ 08_maps/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Define map
+	emails := make(map[string]string)
+
+	// assign kv
+	emails["Abhinav"] = "abhinav@designs.studio"
+	emails["Joe"] = "Joe@JoeDonuts.com"
+	emails["John"] = "John@Doe.com"
+
+	// print map
+	fmt.Println(emails)
+
+	// print one kv
+	fmt.Println(emails["Abhinav"])
+
+	// Delete from map
+	delete(emails, "John")
+
+	// print map
+	fmt.Println(emails)
+}
