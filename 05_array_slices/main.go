6fa2f4c15f412d295dfe5eba166d196e5212f8cd
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-26 09:57:52 +0000

    (feat): add arrays

---
+++ 05_array_slices/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// // arrays
+	// var fruitArr [2]string
+
+	// // assign values
+	// fruitArr[0] = "Apple"
+	// fruitArr[1] = "Orange"
+
+	// declare and assign
+	fruitArr := [2]string{"Apple", "Orange"}
+
+	// print array
+	fmt.Println(fruitArr)
+}
