aac953459705d5e95ad43893ea54872d62a77676
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-26 10:37:50 +0000

    (feat): range and maps

--- 09_range/main.go
+++ 09_range/main.go
@@ -9,4 +9,23 @@ func main() {
 	for i, id := range ids {
 		fmt.Printf("%d - ID: %d\n", i, id)
 	}
+
+	// W/o index
+	for _, id := range ids {
+		fmt.Printf("ID: %d\n", id)
+	}
+
+	// Add ids
+	sum := 0
+	for _, id := range ids {
+		sum += id
+	}
+	fmt.Printf("Sum of ids is %d\n", sum)
+
+	// Range with map
+	emails := map[string]string{"Abhinav": "abhinav@designs.studio", "Joe": "Joe@JoeDonuts.com"}
+
+	for k, v := range emails {
+		fmt.Printf("%s: %s\n", k, v)
+	}
 }
