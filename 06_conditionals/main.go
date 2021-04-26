851961a0de87ed87f8dcb574af91b7010f79ff01
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-26 10:16:52 +0000

    (feat): add switch

--- 06_conditionals/main.go
+++ 06_conditionals/main.go
@@ -6,9 +6,23 @@ func main() {
 	x := 15
 	y := 10
 
+	// if else if else
 	if x < y {
 		fmt.Printf("%d is less than %d\n", x, y)
+	} else if x == y {
+		fmt.Printf("%d is less than %d\n", x, y)
 	} else {
-		fmt.Printf("%d is less than %d\n", y, x)
+		fmt.Printf("%d is equal to %d\n", y, x)
+	}
+
+	color := "red"
+	// switch
+	switch color {
+	case "red":
+		fmt.Println("Color is red")
+	case "blue":
+		fmt.Println("Color is red")
+	default:
+		fmt.Println("Color is rnot blue or red")
 	}
 }
